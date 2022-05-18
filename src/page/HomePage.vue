03a4ef0a5f6cbe6021b675d15ad12147ab567cd1
longlee1107 <59003423+longlee1107@users.noreply.github.com>
2022-05-18 09:14:54 +0700

    first

---
+++ src/page/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>Home Page</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+import VueRouter from "vue-router";
+import Vue from "vue";
+Vue.use(VueRouter);
+export const router = new VueRouter({
+    routes:[
+        {
+            path:'/',
+            name:'home',
+            component:()=>import('../page/HomePage')
+        },
+     
+
+    ],
+    mode: "history"
+})
