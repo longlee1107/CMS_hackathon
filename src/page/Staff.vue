4988bcfdca475b647fc65804e6829045597ab940
CongDuy <rudbich199@gmail.com>
2022-05-24 11:29:50 +0700

    update table staff

--- src/page/Staff.vue
+++ src/page/Staff.vue
@@ -39,318 +39,134 @@
 
           </div>
           <!-- Client Table -->
-          <div class="mt-4 mx-4">
-            <div class="w-full overflow-hidden rounded-lg shadow-xs">
-              <div class="w-full overflow-x-auto">
-                <table class="w-full">
-                  <thead>
-                  <tr
-                      class="
-                        text-xs
-                        font-semibold
-                        tracking-wide
-                        text-left text-gray-500
-                        uppercase
-                        border-b
-                        dark:border-gray-700
-                        bg-gray-50
-                        dark:text-gray-400 dark:bg-gray-800
-                      "
-                  >
-                    <th class="px-4 py-3 w-1">Number</th>
-                    <th class="px-4 py-3">Name</th>
-                    <th class="px-4 py-3">Roll</th>
-                    <th class="px-4 py-3">Note</th>
-                    <th class="px-4 py-3">Status</th>
-                    <th class="px-4 py-3">Date</th>
-                    <th class="px-4 py-3">Request</th>
-                  </tr>
-                  </thead>
-                  <tbody
-                      class="
-                      bg-white
-                      divide-y
-                      dark:divide-gray-700 dark:bg-gray-800
-                    "
-                  >
-                  <tr
-                      v-for="request in requests"
-                      :key="request.id"
-                      class="
-                        bg-gray-50
-                        dark:bg-gray-800
-                        hover:bg-gray-100
-                        dark:hover:bg-gray-900
-                        text-gray-700
-                        dark:text-gray-400
-                      "
-                  >
-                    <td class="px-4 py-3">
-                      <p class="text-center">{{ request.id }}</p>
-                    </td>
-                    <td class="px-4 py-3">
-                      <div class="flex items-center text-sm">
-                        <div
-                            class="
-                              relative
-                              hidden
-                              w-8
-                              h-8
-                              mr-3
-                              rounded-full
-                              md:block
-                            "
-                        >
-                          <img
-                              class="object-cover w-full h-full rounded-full"
-                              src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=200&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjE3Nzg0fQ"
-                              alt=""
-                              loading="lazy"
-                          />
-                          <div
-                              class="absolute inset-0 rounded-full shadow-inner"
-                              aria-hidden="true"
-                          ></div>
-                        </div>
-                        <div>
-                          <p class="font-semibold">{{ user.staffName }}</p>
-                          <p class="text-xs text-gray-600 dark:text-gray-400">
-                            10x Developer
-                          </p>
-                        </div>
-                      </div>
-                    </td>
-                    <td class="px-4 py-3 text-sm">
-                      {{ user.position.positionName }}
-                    </td>
-                    <td class="px-4 py-3 text-sm">{{ request.note }}</td>
-                    <td class="px-4 py-3 text-xs">
-                        <span
-                            class="
-                            px-2
-                            py-1
+          <div class="
+          mt-4
+              gap-2
+              rounded-xl">
+            <RequestPoint/>
+            <div class="mt-4 mx-4" v-if="this.myRequest.length > 0">
+              <div class="w-full overflow-hidden rounded-lg shadow-xs">
+                <div class="w-full overflow-x-auto">
+                  <table class="w-full">
+                    <thead>
+                    <tr
+                        class="
+                            text-xs
                             font-semibold
-                            leading-tight
-                            text-yellow-400
-                            bg-yellow-400
-                            rounded-full
-                            dark:bg-yellow-500 dark:text-green-100"
-                            v-if="request.status === 'PENDING'"
-                        >
-
-                          {{ request.status }}
-                        </span>
-                      <span
-                          class="
-                            px-2
-                            py-1
-                            font-semibold
-                            leading-tight
-                            text-green-700
-                            bg-green-700
-                            rounded-full
-                            dark:bg-green-700 dark:text-green-100"
-                          v-else-if="request.status === 'APPROVED'"
-                      >
-
-                          {{ request.status }}
-                        </span>
-                      <span
-                          class="
-                            px-2
-                            py-1
-                            font-semibold
-                            leading-tight
-                            text-white
-                            bg-red-600
-                            rounded-full
-                            dark:bg-red-600 dark:text-green-100"
-                          v-else
-                      >
-
-                          {{ request.status }}
-                        </span>
-                    </td>
-                    <td class="px-4 py-3 text-sm">
-                      {{ request.timeIn | formatDate }}
-                    </td>
-                    <td
-                        class="px-4 py-3 text-sm"
-                        v-if="request.status === 'PENDING'"
+                            tracking-wide
+                            text-left text-gray-500
+                            uppercase
+                            border-b
+                            dark:border-gray-700
+                            bg-gray-50
+                            dark:text-gray-400 dark:bg-gray-800
+                          "
                     >
-                      <input
-                          type="checkbox"
-                          name=""
-                          id=""
-                          v-model="checkRequest"
-                          :value="request.id"
-                      />
-                    </td>
-                    <td class="px-4 py-3 text-sm" v-else>
-                      <span class="text-green-500 italic">Check</span>
-                    </td>
-                  </tr>
-                  </tbody>
-                </table>
-              </div>
-              <div
-                  class="
-                  grid
-                  px-4
-                  py-3
-                  text-xs
-                  font-semibold
-                  tracking-wide
-                  text-gray-500
-                  uppercase
-                  border-t
-                  dark:border-gray-700
-                  bg-gray-50
-                  sm:grid-cols-9
-                  dark:text-gray-400 dark:bg-gray-800
-                "
-              >
-                <span class="flex items-center col-span-3">
-                  Showing 21-30 of 100
-                </span>
-                <span class="col-span-2"></span>
-                <!-- Pagination -->
-                <span class="flex col-span-4 mt-2 sm:mt-auto sm:justify-end">
-                  <nav aria-label="Table navigation">
-                    <ul class="inline-flex items-center">
-                      <li>
-                        <button
-                            class="
-                            px-3
-                            py-1
-                            rounded-md rounded-l-lg
-                            focus:outline-none focus:shadow-outline-purple
+                      <th class="px-4 py-3 w-1 text-center">Number</th>
+                      <th class="px-4 py-3 text-center">Note</th>
+                      <th class="px-4 py-3 text-center">Created at</th>
+                      <th class="px-4 py-3 text-center">Status</th>
+                      <th class="px-4 py-3 text-center">Other</th>
+                    </tr>
+                    </thead>
+                    <tbody
+                        class="
+                          bg-white
+                          divide-y
+                          dark:divide-gray-700 dark:bg-gray-800
+                        "
+                    >
+                    <tr
+                        v-for="request in myRequest"
+                        :key="request.id"
+                        class="
+                            bg-gray-50
+                            dark:bg-gray-800
+                            hover:bg-gray-100
+                            dark:hover:bg-gray-900
+                            text-gray-700
+                            dark:text-gray-400
                           "
-                            aria-label="Previous"
-                        >
-                          <svg
-                              aria-hidden="true"
-                              class="w-4 h-4 fill-current"
-                              viewBox="0 0 20 20"
+                    >
+                      <td class="px-4 py-3">
+                        <p class="text-center">
+                          {{ request.id }}
+                        </p>
+                      </td>
+                      <td class="px-4 py-3 ">
+                        <div class="flex items-center  text-sm">
+                          <div
+                              class="
+                                  relative
+                                  hidden
+                                  w-44
+                                  h-8
+                                  mr-3
+                                  rounded-full
+                                  md:block
+                                "
                           >
-                            <path
-                                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                                clip-rule="evenodd"
-                                fill-rule="evenodd"
-                            ></path>
-                          </svg>
-                        </button>
-                      </li>
-                      <li>
-                        <button
-                            class="
-                            px-3
-                            py-1
-                            rounded-md
-                            focus:outline-none focus:shadow-outline-purple
-                          "
-                        >
-                          1
-                        </button>
-                      </li>
-                      <li>
-                        <button
-                            class="
-                            px-3
-                            py-1
-                            rounded-md
-                            focus:outline-none focus:shadow-outline-purple
-                          "
-                        >
-                          2
-                        </button>
-                      </li>
-                      <li>
-                        <button
-                            class="
-                            px-3
-                            py-1
-                            text-white
-                            dark:text-gray-800
-                            transition-colors
-                            duration-150
-                            bg-blue-600
-                            dark:bg-gray-100
-                            border border-r-0 border-blue-600
-                            dark:border-gray-100
-                            rounded-md
-                            focus:outline-none focus:shadow-outline-purple
-                          "
-                        >
-                          3
-                        </button>
-                      </li>
-                      <li>
-                        <button
-                            class="
-                            px-3
-                            py-1
-                            rounded-md
-                            focus:outline-none focus:shadow-outline-purple
-                          "
-                        >
-                          4
-                        </button>
-                      </li>
-                      <li>
-                        <span class="px-3 py-1">...</span>
-                      </li>
-                      <li>
-                        <button
-                            class="
-                            px-3
-                            py-1
-                            rounded-md
-                            focus:outline-none focus:shadow-outline-purple
-                          "
-                        >
-                          8
-                        </button>
-                      </li>
-                      <li>
-                        <button
-                            class="
-                            px-3
-                            py-1
-                            rounded-md
-                            focus:outline-none focus:shadow-outline-purple
-                          "
-                        >
-                          9
-                        </button>
-                      </li>
-                      <li>
-                        <button
-                            class="
-                            px-3
-                            py-1
-                            rounded-md rounded-r-lg
-                            focus:outline-none focus:shadow-outline-purple
-                          "
-                            aria-label="Next"
-                        >
-                          <svg
-                              class="w-4 h-4 fill-current"
-                              aria-hidden="true"
-                              viewBox="0 0 20 20"
-                          >
-                            <path
-                                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                                clip-rule="evenodd"
-                                fill-rule="evenodd"
-                            ></path>
-                          </svg>
-                        </button>
-                      </li>
-                    </ul>
-                  </nav>
-                </span>
+                            <div
+                                class="
+                                    absolute
+                                    inset-0
+                                    rounded-full
+                                    shadow-inner
+                                  "
+                                aria-hidden="true"
+                            ></div>
+                          </div>
+                          <div>
+                            <p class="font-semibold">
+                              {{ request.note }}
+                            </p>
+                          </div>
+                        </div>
+                      </td>
+                      <td class="px-4 py-3 text-sm text-center">
+                        {{ request.timeIn | formatDate }}
+                      </td>
+                      <td class="px-4 py-3 text-xs text-center">
+                            <span
+                                class="
+                                px-2
+                                py-1
+                                text-right
+                                font-semibold
+                                leading-tight
+                                text-green-700
+                                bg-green-100
+                                rounded-full
+                                dark:bg-green-700 dark:text-green-100
+                              "
+                            >
+                              {{ request.status }}
+                            </span>
+                      </td>
+                      <td class="text-center">
+                            <span
+                                class="
+                                px-2
+                                py-1
+                                text-right
+                                font-semibold
+                                leading-tight
+                                text-red-700
+                                bg-red-100
+                                rounded-full
+                                dark:bg-red-700 dark:text-red-100
+                              "
+                            >Delete</span
+                            >
+                      </td>
+                    </tr>
+                    </tbody>
+                  </table>
+                </div>
               </div>
+            </div>
+            <div v-else class="italic text-center text-gray-50">
+              No Request Created!
             </div>
           </div>
           <!-- ./Client Table -->
@@ -421,6 +237,7 @@ import {staffService} from "@/service/staffService"
 import {authService} from "@/service/authService"
 import {UserService} from "@/service/UserService";
 import CompoStaffPage from "@/components/CompoStaffPage";
+import {timeKeepingService} from "@/service/timeKeepingService";
 
 export default {
   name: "Staff",
@@ -433,9 +250,19 @@ export default {
       showModalLateForWork: false,
       contentTakeOff: "",
       dataUser: [],
+      currentUser: {},
+      myRequest: {},
     }
   },
   methods: {
+    async getProfileOfCurrentUser() {
+      const res = await UserService.getProfileOfCurrentUser();
+      this.currentUser = res.data.data;
+    },
+    async getMyRequest() {
+      const response = await timeKeepingService.getMyRequest();
+      this.myRequest = response.data.data.content;
+    },
     async Timekeeping() {
       if (this.date === "") {
         alert("Please select a date");
@@ -503,6 +330,8 @@ export default {
   },
   mounted() {
     this.getUser();
+    this.getProfileOfCurrentUser();
+    this.getMyRequest();
   }
 }
 </script>
