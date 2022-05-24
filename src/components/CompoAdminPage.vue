500d5d7ac063f1669c687484ab84e03bcb84a444
CongDuy <rudbich199@gmail.com>
2022-05-24 11:16:22 +0700

    update admin

--- src/components/CompoAdminPage.vue
+++ src/components/CompoAdminPage.vue
@@ -78,7 +78,7 @@
               </a>
             </li>
           </router-link>
-          <router-link to="/leader">
+          <router-link to="/admin/attendance">
             <li>
               <a href="#"
                  class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-800 dark:hover:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 dark:hover:border-gray-800 pr-6">
